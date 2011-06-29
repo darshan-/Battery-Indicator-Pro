5fe4f70c25b97168a1e63c9a2fe0b1c7f7d60521
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-06-29 00:42:03 +0000

    Open browser for 'Get Plugins'
    git-svn-id: file:///home/darshan/svn-repo-clone-of-bi-on-google-code/trunk@408 fb11ccbe-488a-4026-b0b0-a5194d29edc0

--- src/com/darshancomputing/BatteryIndicatorPro/SettingsActivity.java
+++ src/com/darshancomputing/BatteryIndicatorPro/SettingsActivity.java
@@ -360,6 +360,11 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
             startActivity(intent);
 
             return true;
+        case R.id.menu_get_plugins:
+            startActivity(new Intent(Intent.ACTION_VIEW, android.net.Uri.parse
+                                     ("http://www.darshancomputing.com/android/battery_indicator/")));
+                                  // TODO: Set this up with a dc.com URL redirecting to current URL
+            return true;
         case android.R.id.home:
             startActivity(new Intent(this, BatteryIndicator.class));
             return true;
