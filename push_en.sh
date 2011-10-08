eb033bca374aa365a0142ac0a58106775a9c19ab
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-10-08 15:49:43 -0700

    Update URL to ATH for push_en.sh

--- push_en.sh
+++ push_en.sh
@@ -2,7 +2,7 @@
 
 scp -q res/values/strings.xml linode:ath/incoming/en.xml
 
-wget --quiet -O /dev/null --ignore-length --post-data="" http://linode/ath/bi/load_new_en
+wget --quiet -O /dev/null --ignore-length --post-data="" http://ath.darshancomputing.com/bi/load_new_en
 
 if [ "$?" -ne "0" ]; then
     echo "Sorry, something didn't work!"
