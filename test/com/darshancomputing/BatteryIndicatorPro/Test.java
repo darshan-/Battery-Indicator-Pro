728377f12938d04f94c9e029c7398a20a194156a
Darshan-Josiah Barber <darshan@darshancomputing.com>
2013-04-30 18:45:38 -0700

    Updated predictor testing code.

--- test/com/darshancomputing/BatteryIndicatorPro/Test.java
+++ test/com/darshancomputing/BatteryIndicatorPro/Test.java
@@ -23,11 +23,12 @@ public class Test {
         pc = new PredictorCore(-1, -1, -1, -1);
         bi = new BatteryInfo();
         now = 0l;
+
+        /*
         bi.percent = 98;
         pc.update(bi, now);
         print();
 
-        /*
         now += 60 * 1000;
         bi.percent = 98;
         bi.plugged = BatteryInfo.PLUGGED_AC;
@@ -71,35 +72,27 @@ public class Test {
         print();
         */
 
-        for (int i = 0; i < 10 * 20; i++) {
-            if (i % 20 == 0) bi.percent -= 1;
-            now += 3 * 1000;
+        int level_by_minute[] = {98, 98,
+                                 97, 97,
+                                 96, 96,
+                                 95,
+                                 94, 94, 94, 94, 94, 94, 94, 94, 94, 94,
+                                 96, 96,
+                                 95, 95, 95, 95,
+                                 94, 94,
+                                 93, 93,
+                                 92, 92, 92, 92, 92,
+                                 91, 91, 91, 91, 91,
+                                 90, 90,
+                                 89,
+                                 88, 88, 88, 88, 88, 88, 88,
+                                 87
+        };
+
+        for (int l : level_by_minute) {
+            bi.percent = l;
             pc.update(bi, now);
-            print();
-        }
-
-        for (int i = 0; i < 50; i++) {
-            now += 3 * 1000;
-            pc.update(bi, now);
-            print();
-        }
-
-        for (int i = 0; i < 20; i++) {
-            if (i % 20 == 0) bi.percent -= 1;
-            now += 3 * 1000;
-            pc.update(bi, now);
-            print();
-        }
-
-        bi.percent = 88;
-        now += 60 * 1000;
-        pc.update(bi, now);
-        print();
-
-        for (int i = 0; i < 40 * 2; i++) {
-            if (i % 2 == 0) bi.percent -= 1;
             now += 60 * 1000;
-            pc.update(bi, now);
             print();
         }
     }
@@ -107,16 +100,22 @@ public class Test {
     private static void print() {
         switch(bi.status) {
         case  BatteryInfo.STATUS_UNPLUGGED:
-            System.out.println("" + (now/1000) + ": " + bi.percent + "%; drained in " + pc.last_seconds_remaining + " seconds.");
+            System.out.println("" + (now/60000.0) + ": " + bi.percent + "%; drained in " + prettyTimeRemaining(bi));
             break;
         case  BatteryInfo.STATUS_CHARGING:
-            System.out.println("" + (now/1000) + ": " + bi.percent + "%; charged in " + pc.last_seconds_remaining + " seconds.");
+            System.out.println("" + (now/60000.0) + ": " + bi.percent + "%; charged in " + prettyTimeRemaining(bi));
             break;
         case  BatteryInfo.STATUS_FULLY_CHARGED:
-            System.out.println("" + (now/1000) + ": " + bi.percent + "%; fully charged.");
+            System.out.println("" + (now/60000.0) + ": " + bi.percent + "%; fully charged.");
             break;
         default:
-            System.out.println("" + (now/1000) + ": unknown status: " + bi.status);
+            System.out.println("" + (now/60000.0) + ": unknown status: " + bi.status);
         }
+    }
+
+    private static String prettyTimeRemaining(BatteryInfo info) {
+        return "" +
+            (info.prediction.days * 24 + info.prediction.hours) + "h " +
+            info.prediction.minutes + "m";
     }
 }
