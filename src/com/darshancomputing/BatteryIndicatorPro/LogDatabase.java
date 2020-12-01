58fee9f7086d0bfe975daf3904c5f1db47f8c3ad
Darshan-Josiah Barber <darshan@darshancomputing.com>
2020-11-30 23:12:06 -0800

    Updated copyright years.

--- src/com/darshancomputing/BatteryIndicatorPro/LogDatabase.java
+++ src/com/darshancomputing/BatteryIndicatorPro/LogDatabase.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2010-2017 Darshan Computing, LLC
+    Copyright (c) 2010-2020 Darshan Computing, LLC
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
