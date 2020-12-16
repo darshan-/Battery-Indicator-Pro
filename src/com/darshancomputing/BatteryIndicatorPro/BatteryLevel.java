d187264f749c769832fb075931c795c0d78d91d7
Darshan-Josiah Barber <darshan@darshancomputing.com>
2020-12-16 11:17:34 -0800

    Experimenting with new (flatter?  more modern?  simpler?) battery level graphic.  Pretty sure I want to go this direction.  May bring old one back as an option for Pro users, but not sure.

--- src/com/darshancomputing/BatteryIndicatorPro/BatteryLevel.java
+++ src/com/darshancomputing/BatteryIndicatorPro/BatteryLevel.java
@@ -25,17 +25,25 @@ import android.graphics.RectF;
 import android.util.DisplayMetrics;
 
 class BatteryLevel {
-    private int width, top_h, body_h;
     private Canvas canvas;
-    private Paint fill_paint, bitmap_paint;
-    private static Bitmap battery_top, battery_body, battery;
+    private Paint paint;
+    private static Bitmap battery;
     private int mLevel, mColor;
 
     static final int SIZE_LARGE = 1;
     static final int SIZE_NOTIFICATION = 4;
 
+
+    private final int WIDTH = 240;
+    private final int BODY_HEIGHT = 420;
+    private final int TOP_WIDTH = 100;
+    private final int TOP_HEIGHT = 30;
+    private final int TOTAL_HEIGHT = BODY_HEIGHT + TOP_HEIGHT;
+    private final float STROKE_WIDTH = 20f;
+
     private static BatteryLevel[] instances = new BatteryLevel[]{null, null, null, null, null}; // So that [1], [2], and [4] exist
 
+    // Get rid of inSampleSize, I think!
     public static BatteryLevel getInstance(Context context, int inSampleSize) {
         if (inSampleSize < 0 || inSampleSize >= instances.length)
             return null;
@@ -53,38 +61,19 @@ class BatteryLevel {
     private BatteryLevel(Context context, int inSampleSize) {
         Resources res = context.getResources();
 
-        //BitmapFactory bf = new BitmapFactory();
-        BitmapFactory.Options bfo = new BitmapFactory.Options();
-        bfo.inDensity = DisplayMetrics.DENSITY_DEFAULT;
-        bfo.inScaled = false;
-        bfo.inTargetDensity = DisplayMetrics.DENSITY_DEFAULT;
-        bfo.inSampleSize = inSampleSize;
-
-        battery_top    = BitmapFactory.decodeResource(res, R.drawable.empty_battery_top   , bfo);
-        battery_body   = BitmapFactory.decodeResource(res, R.drawable.empty_battery_body  , bfo);
-
-           width = battery_top.getWidth();
-           top_h = battery_top.getHeight();
-          body_h = battery_body.getHeight();
-
         canvas = new Canvas();
 
-        battery = Bitmap.createBitmap(width, top_h + body_h, Bitmap.Config.ARGB_8888);
+        battery = Bitmap.createBitmap(WIDTH, TOTAL_HEIGHT, Bitmap.Config.ARGB_8888);
         battery.setDensity(DisplayMetrics.DENSITY_DEFAULT);
         canvas.setBitmap(battery);
 
-        fill_paint = new Paint();
-        //fill_paint.setColor(0xaa33b5e5);
-        mColor = 0xaa33b5e5;
-        fill_paint.setAntiAlias(true);
-        fill_paint.setStrokeCap(Paint.Cap.ROUND);
-        fill_paint.setStrokeJoin(Paint.Join.ROUND);
-        fill_paint.setStyle(Paint.Style.FILL);
-        fill_paint.setDither(true);
-
-        bitmap_paint = new Paint();
-        bitmap_paint.setAntiAlias(true);
-        bitmap_paint.setDither(true);
+        paint = new Paint();
+        mColor = 0xff586fb8;
+        paint.setAntiAlias(true);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setDither(true);
+        paint.setStrokeWidth(STROKE_WIDTH);
     }
 
     public void setColor(int color) {
@@ -94,20 +83,27 @@ class BatteryLevel {
 
     public void setLevel(int level) {
         if (level < 0) level = 0; // I suspect we might get called with -1 in certain circumstances
+
         mLevel = level;
 
-        int rect_top = top_h + (body_h * (100 - level) / 100);
-        int rect_bottom = top_h + body_h;
-
-        RectF body_rect = new RectF(0, rect_top, width, rect_bottom);
-
+        int sw = (int) STROKE_WIDTH;
+        RectF outline_rect = new RectF(sw/2, TOP_HEIGHT + sw/2, WIDTH-sw/2, TOTAL_HEIGHT-sw/2);
         canvas.drawColor(Color.TRANSPARENT, android.graphics.PorterDuff.Mode.CLEAR);
 
-        fill_paint.setColor(mColor);
-        canvas.drawRoundRect(body_rect, 7.5f, 7.5f, fill_paint);
+        paint.setColor(mColor);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRoundRect(outline_rect, sw, sw, paint);
 
-        canvas.drawBitmap(battery_top   , 0, 0             , bitmap_paint);
-        canvas.drawBitmap(battery_body  , 0, top_h         , bitmap_paint);
+        int top = TOP_HEIGHT + sw/2 + (BODY_HEIGHT * (100-level) / 100);
+        RectF fill_rect = new RectF(sw/2, top, WIDTH-sw/2, TOTAL_HEIGHT-sw/2);
+        // Draw line across top to level out rounding?
+
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRoundRect(fill_rect, sw, sw, paint);
+
+        int top_left = (WIDTH - TOP_WIDTH) / 2;
+        RectF top_rect = new RectF(top_left, 0, top_left+TOP_WIDTH, TOP_HEIGHT+sw);
+        canvas.drawRoundRect(top_rect, sw, sw, paint);
     }
 
     Bitmap getBitmap() {
